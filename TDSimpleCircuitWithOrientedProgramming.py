741f1d56d3b7771d6cafb99f9f105c50d8a47a23
paullebric <paullebric@gmail.com>
2024-05-21 17:20:07 +0200

    Create TDSimpleCircuitWithOrientedProgramming.py

---
+++ TDSimpleCircuitWithOrientedProgramming.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+@dataclass
+class rabbit :
+    pass
+@dataclass
+class wolf :
+    pass        
+@dataclass
+class interaction :
+    pass
+@dataclass
+class circuit :
+    pass
+
+
