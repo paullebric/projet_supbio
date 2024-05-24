dfaf6def83f3b7822c53e73d4b411c3202658209
paullebric <paullebric@gmail.com>
2024-05-24 10:35:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
 #alicia et alice les grosses nulles elles verront jamais ce message
+from dataclasses import dataclass
+
+
+@dataclass
+class Promoter :
+    quantity : float = 0
+@dataclass
+class ProteinY :
+    quantity : float = 0
+@dataclass
+class EnzymeA :
+    quantity : float = 0
+@dataclass
+class SubstrateA :
+    quantity : float = 0
+@dataclass
+class ProductB :
+    quantity : float = 0
+@dataclass
+class Circuit :
+    Cytoplasm : list = [object]
+    Feedback : list = []
+    def __init__(self):
+        self.Enzymes = []
+        self.Feedback = []
+    def add(self,x):
+        self.Cytoplasm.append(x)
+
+circuit = Circuit()
+circuit.add("Enzyme A"
+, EnzymeA(quantity=10))
+circuit.add("Protein Y"
+, ProteinY(quantity=10))
+circuit.add("Substrate A"
+, SubstrateA(quantity=100))
+circuit.add("Product B"
+, ProductB(quantity=0))
+circuit.simulate(steps=100)
