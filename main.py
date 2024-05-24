93f41a44572e396ce8db7e9fadb3143f85ac7451
paullebric <paullebric@gmail.com>
2024-05-24 12:00:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 #alicia et alice les grosses nulles elles verront jamais ce message
 from dataclasses import dataclass
 import pandas as pd
+import matplotlib.pyplot as plt
 active = True
 inactive = False
-k = 1
+k = 0.0001
+activation_treshold=50
 @dataclass
 class ProteinY :
     quantity : float = 0
     name : str = "Protein Y"
     state : bool = True
-    def update(self,data):
-        pass
+    def update(self,Cytoplasm):
+        if Cytoplasm["Substrate A"].quantity > activation_treshold :
+            self.quantity *=1.1 #ici l'augmentation de la prot Y est de 10% on peut changer et choisir comme on veut
 @dataclass
 class EnzymeA :
     quantity : float = 0
@@ -29,9 +32,8 @@ class ProductB :
     quantity : float = 0
     name : str = "Product B"
     def update(self,Cytoplasm):
-        print(Cytoplasm["Enzyme A"].quantity,Cytoplasm["Substrate A"].quantity)
-        Rate = k* [Cytoplasm["Enzyme A"].quantity] * [Cytoplasm["Substrate A"].quantity]
-        SubstrateA.quantity -=Cytoplasm["Substrate A"].quantity * Rate
+        Rate = k* Cytoplasm["Enzyme A"].quantity * Cytoplasm["Substrate A"].quantity
+        Cytoplasm["Substrate A"].quantity -=Cytoplasm["Substrate A"].quantity * Rate #ici je considère que 1 substrat A donne 1 produit B on pourra changer plus tard
         self.quantity +=Cytoplasm["Substrate A"].quantity * Rate
 
 @dataclass
@@ -49,14 +51,13 @@ class Circuit :
         for x in self.Cytoplasm.keys():
             data[x] = []
         for i in range(steps):
-            print(i)
-            for protein in self.Cytoplasm:
-                protein[1].update(self.Cytoplasm)
-            for protein in self.Cytoplasm:
-                data[protein[1].name].append(protein[1].population)
+            for protein in self.Cytoplasm.values():
+                protein.update(self.Cytoplasm)
+            for protein in self.Cytoplasm.values():
+                data[protein.name].append(protein.quantity)
         df = pd.DataFrame(data)
         df.plot()
-
+        plt.show()
 
 
 circuit = Circuit()
