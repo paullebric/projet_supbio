23c0d7c3ee2a474fe0a2a6e29ae15f6419b36454
paullebric <paullebric@gmail.com>
2024-05-31 14:39:48 +0200

    Create new_main_paul.py

---
+++ new_main_paul.py
@@ -0,0 +1,100 @@
+#alicia et alice les grosses nulles elles verront jamais ce message
+from dataclasses import dataclass
+import pandas as pd
+import matplotlib.pyplot as plt
+
+k = 0.07 #rate
+environment_signal=25
+activation_treshold=23
+feedback_treshold = 20
+basal_rate=1.10 #a peut pres 1.115 pour enzyme A
+@dataclass
+class ProteinY :
+    quantity : float = 0
+    name : str = "Protein Y"
+    state :bool = True
+    prod_rate : float = basal_rate
+    def update(self,Cytoplasm):
+        if Cytoplasm["Substrate A"].quantity>environment_signal:
+                    self.quantity *= self.prod_rate
+        else :
+                    self.quantity -= Cytoplasm["Enzyme A"].quantity/10#basal rate changeables
+    
+    
+@dataclass
+class EnzymeA :
+    quantity : float = 0
+    name : str = "Enzyme A"
+    prod_rate : float = 0.01
+    def update(self,Cytoplasm):
+        '''
+        If Y is active: production of Enzyme A is activated.
+        If Y is inactive: production of Enzyme A is inhibited.
+        '''
+        if Cytoplasm["Protein Y"].state ==True :
+            self.quantity+=(Cytoplasm["Protein Y"].quantity-self.quantity)/10
+
+@dataclass
+class SubstrateA :
+    quantity : float = 0
+    name : str = "Substrate A"
+    def update(self,data):
+        self.quantity += 5.6
+
+@dataclass
+class ProductB :
+    quantity : float = 0
+    name : str = "Product B"
+    def update(self,Cytoplasm):
+        pass
+@dataclass
+class Transformation:
+    Substrate : object
+    Product : object
+    Enzyme : object
+    rate: float = k
+    def update(self):
+#Enzyme A catalyzes the chemical reaction: Substrate A →Product B. The rate of this reaction depends on the concentration of available Enzyme A and Substrate A.
+        delta = self.rate * self.Substrate.quantity * self.Enzyme.quantity
+        self.Substrate.quantity -= delta
+        self.Product.quantity += delta/10 #pour 10 substrat formation de 1 produit (arbitraire)
+
+
+@dataclass
+class Circuit :
+    Cytoplasm : dict
+    Transformations : list
+    def __init__(self):
+        self.Cytoplasm = {}
+        self.Transformations =[]
+    def add(self,name,objet):
+        self.Cytoplasm[name]=objet
+    def add_transformation(self):
+        Substrate=self.Cytoplasm["Substrate A"]
+        Product=self.Cytoplasm["Product B"]
+        Enzyme = self.Cytoplasm["Enzyme A"]
+        i = Transformation(Substrate=Substrate, Product=Product,Enzyme=Enzyme)
+        self.Transformations.append(i)
+    def simulate(self, steps):
+        self.add_transformation()
+        data = {}
+        for x in self.Cytoplasm.keys():
+            data[x] = []
+        for i in range(steps):
+            for protein in self.Cytoplasm.values():
+                data[protein.name].append(protein.quantity)
+            for transfo in self.Transformations:
+                transfo.update()
+            for protein in self.Cytoplasm.values():
+                protein.update(self.Cytoplasm)
+        df = pd.DataFrame(data)
+        df.plot()
+        plt.show()
+
+
+circuit = Circuit()
+circuit.add("Enzyme A", EnzymeA(quantity=5))
+circuit.add("Protein Y", ProteinY(quantity=5))
+circuit.add("Substrate A", SubstrateA(quantity=50))
+circuit.add("Product B", ProductB(quantity=0))
+circuit.simulate(steps=1000)
